aaed996ec7705eda7ecc6aaa4bda10dd45777175
KevinEBM <kebuitrago_506@unihumboldt.edu.co>
2024-09-02 23:28:08 -0500

    Update programa4.js

--- seguimiento3/programa4.js
+++ seguimiento3/programa4.js
@@ -1,1 +1,13 @@
+let semaforo = ["rojo", "amarillo", "verde"]
+let color = 0
+function semaforo2(){
+    alert(`Es ${semaforo[color]}`)
 
+    color = (color +1)% semaforo.length
+}
+let intervaloId = setInterval(semaforo2,10000 )
+
+setTimeout(() => {
+    clearInterval(intervaloId);
+    alert("Ha sido detenido");
+}, 10000);
